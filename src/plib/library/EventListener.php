14f7321d764607b82f4f55dae5a74fbe97e58858
Bozhidar Slaveykov <bobi@microweber.com>
2023-01-20 13:17:17 +0200

    Update EventListener.php

--- src/plib/library/EventListener.php
+++ src/plib/library/EventListener.php
@@ -31,13 +31,58 @@ class Modules_Skeleton_EventListener implements EventListener
                 if (!$domain->hasHosting()) {
                     return false;
                 }
-
-
-                
-
+                if (!$this->checkSsl($domain->getName())) {
+                    $this->addDomainEncryption($domain);
+                } else {
+                    // Domain already have a SSL.
+                }
 
             break;
         }
+    }
+
+    private function checkSsl($domainName)
+    {
+        $g = @stream_context_create (array("ssl" => array("capture_peer_cert" => true)));
+        $r = @stream_socket_client("ssl://www.".$domainName.":443", $errno, $errstr, 30,
+            STREAM_CLIENT_CONNECT, $g);
+        $cont = @stream_context_get_params($r);
+        if (isset($cont["options"]["ssl"]["peer_certificate"])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function addDomainEncryption($domain)
+    {
+        $artisan = false;
+
+        $sslEmail = 'admin@microweber.com';
+
+        $encryptOptions = [];
+        $encryptOptions[] = '--domain';
+        $encryptOptions[] = $domain->getName();
+        $encryptOptions[] = '--email';
+        $encryptOptions[] = $sslEmail;
+
+        // Add SSL
+        try {
+            //Modules_Microweber_Log::debug('Start installing SSL for domain: ' . $domain->getName() . '; SSL Email: ' . $sslEmail);
+
+            $artisan = \pm_ApiCli::call('extension', array_merge(['--exec', 'letsencrypt', 'cli.php'], $encryptOptions), \pm_ApiCli::RESULT_FULL);
+
+            //Modules_Microweber_Log::debug('Encrypt domain log for: ' . $domain->getName() . '<br />' . $artisan['stdout']. '<br /><br />');
+            //Modules_Microweber_Log::debug('Success instalation SSL for domain: ' . $domain->getName());
+
+        } catch(\Exception $e) {
+
+            //Modules_Microweber_Log::debug('Can\'t install SSL for domain: ' . $domain->getName());
+            //Modules_Microweber_Log::debug('Error: ' . $e->getMessage());
+
+        }
+
+        return $artisan;
     }
 }
 
